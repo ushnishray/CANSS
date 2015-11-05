ad9ead4839b70bb0ae25f0fcc597de12d2164b4b
ushnishray <ushnish.qmc@gmail.com>
2015-11-04 21:56:42 -0500

    Updated headers

--- observables/measurables.h
+++ observables/measurables.h
@@ -6,16 +6,6 @@
  *
  	Copyright (c) 2014 Ushnish Ray
 	All rights reserved.
-
-	This program and the accompanying materials are made available under explicit agreement
-	between Ushnish Ray and the end user. You may not redistribute the code and
-	accompanying material to anyone.
-
-	On the event that the software is used to generate data that is used implicitly or explicitly
-	for research purposes, proper acknowledgment must provided  in the citations section of
-	publications.
-
-	This software is cannot be used for commercial purposes in any way whatsoever.
  */
 
 #ifndef MEASURABLES_H_
