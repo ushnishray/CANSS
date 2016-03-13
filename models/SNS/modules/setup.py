ea2cf839115de0fba4b93856fa6f42f5b885ddbe
ushnishray <ushnish.qmc@gmail.com>
2016-03-12 21:43:47 -0500

    Updated setup module to be general

--- models/SNS/modules/setup.py
+++ models/SNS/modules/setup.py
@@ -6,10 +6,11 @@ betas = fin.read().split('\n')
 fin.close()
 
 w = ''
-bexec = '/home/ushnish/research/dmcsns/models/SNS/d1b/DEX.branch'
+bexec = '/home/ushnish/research/Development/checkouts/DMCsns/cppmods/models/DBM/d1/DEX.branch'
 i = 0
 
 nproc = 2
+tproc = 8
 for beta in betas:
 	try:
 		float(beta)
@@ -18,9 +19,9 @@ for beta in betas:
 		os.system('cp -r params ' + sdir + '/')
 
 		w += 'cd ../' + sdir + '\n'
-		w += 'mpirun -np 2 ' + bexec + ' ' + './params/params.txt ' + str(beta) + ' >> out &\n\n'
+		w += 'mpirun -np ' + str(nproc) + ' ' + bexec + ' ' + './params/param.txt ' + str(beta) + ' >> out &\n\n'
 		i += 1
-		if((i*nproc)%30 == 0):
+		if((i*nproc)%tproc == 0):
 			w += 'wait\n'
 	except:
 		pass		
